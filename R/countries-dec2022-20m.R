3d570ef31c244b6080318a77e26b59905a7b1afc
jimgar <jimmahgee@gmail.com>
2023-03-11 23:57:48 +0000

    Add countries_dec2022_20m

---
+++ R/countries-dec2022-20m.R
@@ -0,0 +1,20 @@
+#' UK Countries Boundaries December 2022
+#'
+#' This file contains the digital vector boundaries for Countries in the United Kingdom as at December 2022. The boundaries available are: (GBC) Generalised (20m) - clipped to the coastline (Mean High Water mark). Contains both Ordnance Survey and ONS Intellectual Property Rights. 
+#'
+#' @format Simple feature collection with 4 features and 10 fields.
+#' \describe{
+#'   \item{FID}{ID}
+#'   \item{CTRY22CD}{2022 Country Code}
+#'   \item{CTRY22NM}{2022 Country Name} 
+#'   \item{CTRY22NMW}{2022 Country Name (Welsh)} 
+#'   \item{BNG_E}{Easting} 
+#'   \item{BNG_N}{Northing}
+#'   \item{LONG}{Longitude} 
+#'   \item{LAT}{Latitude} 
+#'   \item{Shape__Area}{Surface area (square meters)} 
+#'   \item{Shape__Length}{Boundary length (meters)} 
+#'   \item{geometry}{Geometry type of MULTIPOLYGON} 
+#' }
+#' @source \url{https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Countries_December_2022_UK_BGC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson}
+"countries_dec2022_20m"
---
+++ data-raw/countries-dec2022-20m.R
@@ -0,0 +1,11 @@
+source("data-raw/api-calls.R")
+library(sf)
+
+# Collect data
+api_url <- extract_api_url("countries_dec2022_20m")
+
+countries_dec2022_20m <- sf::read_sf(api_url) |>
+  dplyr::select(-GlobalID)
+
+# Write to data folder
+usethis::use_data(countries_dec2022_20m, overwrite = TRUE)
