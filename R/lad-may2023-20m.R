07ad5b2d3f26eb6fabca0266918a0d1af467cac8
jimgar <jimmahgee@gmail.com>
2023-06-20 22:37:23 +0100

    Added May 2023 LAD boundaries

---
+++ R/lad-may2023-20m.R
@@ -0,0 +1,20 @@
+#' UK Local Authority District Boundaries May 2023
+#'
+#' Boundaries for Local Authority Districts in the United Kingdom as at May 2023. The boundaries available are: (BGC) Generalised (20m) - clipped to the coastline (Mean High Water Mark). Contains both Ordnance Survey and ONS Intellectual Property Rights. 
+#'
+#' @format Simple feature collection with 361 features and 10 fields.
+#' \describe{
+#'   \item{FID}{Feature ID}
+#'   \item{LAD23CD}{2023 Local Authority District Code}
+#'   \item{LAD23NM}{2023 Local Authority District Name} 
+#'   \item{LAD23NMW}{2023 Local Authority District Name in Welsh} 
+#'   \item{BNG_E}{Easting} 
+#'   \item{BNG_N}{Northing}
+#'   \item{LONG}{Longitude} 
+#'   \item{LAT}{Latitude} 
+#'   \item{Shape__Area}{Surface area (square meters)} 
+#'   \item{Shape__Length}{Boundary length (meters)} 
+#'   \item{geometry}{Geometry type of MULTIPOLYGON} 
+#' }
+#' @source \url{https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Local_Authority_Districts_May_2023_UK_BGC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson}
+"lad_may2023_20m"
---
+++ data-raw/lad-may2023-20m.R
@@ -0,0 +1,11 @@
+source("data-raw/api-calls.R")
+library(sf)
+
+# Collect data
+api_url <- extract_api_url("lad_may2023_20m")
+
+lad_may2023_20m <- sf::read_sf(api_url) |>
+  dplyr::select(-GlobalID)
+
+# Write to data folder
+usethis::use_data(lad_may2023_20m, overwrite = TRUE)
